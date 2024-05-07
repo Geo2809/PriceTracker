8611c0c7010d48f37cc04fe76fd2256567f819bf
George <george.9409@yahoo.ro>
2024-05-07 21:48:45 +0300

    Add data extraction process into a function.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import lxml
-url = 'https://www.emag.ro/telefon-mobil-apple-iphone-15-pro-max-256gb-5g-blue-titanium-mu7a3rx-a/pd/DHLH93YBM/'
+#url = 'https://www.emag.ro/telefon-mobil-apple-iphone-15-pro-max-256gb-5g-blue-titanium-mu7a3rx-a/pd/DHLH93YBM/'
+url = 'https://www.emag.ro/laptop-apple-macbook-pro-14-cu-procesor-apple-m3-8-nuclee-cpu-si-10-nuclee-gpu-512gb-ssd-space-grey-int-kb-mtl73ro-a/pd/DKSN2DYBM/'
 
-headers = {
+def get_link_data(url):
+    headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     'Accept-Language': 'ro'
-}
+    }
 
 
-r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=headers)
 
-soup = BeautifulSoup(r.text, 'lxml')
-product_name = soup.find('h1', class_='page-title').text.strip()
-price = soup.find('p', class_='product-new-price has-deal').text.strip()[:-7]
-price = float(price)
+    soup = BeautifulSoup(r.text, 'lxml')
+    product_name = soup.find('h1', class_='page-title').text.strip()
+    price = soup.find('p', class_='product-new-price has-deal').text.strip()[:-7]
+    price = float(price)
+    return product_name, price
 
