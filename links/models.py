84dcdfe7be87d46e50f5199ab8d6d8fa6b392b5c
George <george.9409@yahoo.ro>
2024-05-08 15:09:43 +0300

    Create Link model in links app.

--- links/models.py
+++ links/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    name = models.CharField(max_length=255, blank=True)
+    url = models.URLField()
+    current_price = models.DecimalField(max_digits=10, decimal_places=3, blank=True)
+    old_price = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    price_difference = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ['price_difference', '-created']
+
+ 
+    
+    
